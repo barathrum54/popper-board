b24539d61daf62234c0f2bb6ce4110728816fecb
barathrum54 <tahabdurmus0@gmail.com>
2022-12-14 11:51:17 +0300

    -

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import CreateView from '@/views/HomeView.vue'
+import CreateView from '@/views/CreateView.vue'
 
 const routes = [
   {
